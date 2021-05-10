c06eefdef0458743be79125d90547e7616cebc8d
mustafahassoon@gmail.com <mustafahassoon@gmail.com>
2021-05-10 07:46:31 +0200

    first draft task 5.3

--- components/Chat.js
+++ components/Chat.js
@@ -1,21 +1,73 @@
 import React, { Component } from 'react';
-import { StyleSheet, ImageBackground, Text, Button, View } from 'react-native';
+
+//importing gifted liberary
+import { GiftedChat, Bubble } from 'react-native-gifted-chat'
+
+
+import { StyleSheet, ImageBackground, Text, Button, View, Platform, KeyboardAvoidingView } from 'react-native';
 
 export default class Chat extends React.Component {
 
-  // constructor() {
-  //   super();
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    messages: [],
+  }
 
-  // }
 
 
-  // pulling in information from Start.js name/color
+  //putting the users name in navigation bar
   // static navigationOptions = ({ navigation }) => {
   //   return {
-  //     name: navigation.state.params.name,
-  //     color: navigation.state.params.color,
-  //   };
-  // };
+  //     title: navigation.state.params.name,
+  //   }
+  // }
+
+  componentDidMount() {
+    console.log(this.state);
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          // text: this.props.navigation.state.params.name + ' has entered the chat',
+          text: ' someone' + ' has entered the chat',
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+  //changing the sender’s speech bubble 
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          }
+        }}
+      />
+    )
+  }
+
   render() {
 
     let name = this.props.route.params.name;
@@ -23,9 +75,21 @@ export default class Chat extends React.Component {
 
     this.props.navigation.setOptions({ title: name });
 
+    // <View style={[styles.container, { backgroundColor: color }]}>
+
     return (
-      <View style={[styles.container, { backgroundColor: color }]}>
-        <Text style={{ color: '#FFFFFF' }}>This is your chat screen</Text>
+      <View style={{ flex: 1, backgroundColor: color }}>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {/* fixing android keyboard */}
+        { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null
+        }
       </View>
     );
   }
